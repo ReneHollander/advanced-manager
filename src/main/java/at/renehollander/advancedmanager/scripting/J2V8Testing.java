bd87269380d45b91bd8440afb9c7502e9b3856de
ReneHollander <rene.hollander@hotmail.de>
2015-07-26 04:17:51 +0200

    playing around with j2v8

---
+++ src/main/java/at/renehollander/advancedmanager/scripting/J2V8Testing.java
@@ -0,0 +1,33 @@
+package at.renehollander.advancedmanager.scripting;
+
+import com.eclipsesource.v8.V8;
+import com.eclipsesource.v8.V8Array;
+import com.eclipsesource.v8.V8Function;
+import com.eclipsesource.v8.V8Object;
+
+public class J2V8Testing {
+
+    public J2V8Testing() {
+        V8 runtime = V8.createV8Runtime();
+        runtime.registerJavaMethod(this::test, "test");
+        runtime.registerJavaMethod(this::print, "print");
+        runtime.executeVoidScript("test(function() { print('lol'); });");
+        runtime.release();
+    }
+
+    public void print(V8Object receiver, V8Array parameters) {
+        System.out.println(parameters.get(0));
+    }
+
+    public void test(V8Object receiver, V8Array parameters) {
+        V8Function cb = (V8Function) parameters.get(0);
+        cb.call(receiver, null);
+        cb.release();
+    }
+
+    public static void main(String[] args) {
+        new J2V8Testing();
+    }
+
+
+}
